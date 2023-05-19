2b12165105ccda74e1c3df1b1b07fdd61eaf8380
yach36 <yach0306@gmail.com>
2023-05-19 13:31:56 +0900

    chore: article関連ファイル整理

--- src/article/article.resolver.ts
+++ src/article/article.resolver.ts
@@ -1,34 +1,12 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { ArticleService } from './article.service';
-import { CreateArticleInput } from './dto/create-article.input';
-import { UpdateArticleInput } from './dto/update-article.input';
 
 @Resolver('Article')
 export class ArticleResolver {
   constructor(private readonly articleService: ArticleService) {}
 
-  @Mutation('createArticle')
-  create(@Args('createArticleInput') createArticleInput: CreateArticleInput) {
-    return this.articleService.create(createArticleInput);
-  }
-
-  @Query('article')
-  findAll() {
-    return this.articleService.findAll();
-  }
-
   @Query('article')
   findOne(@Args('id') id: number) {
-    return this.articleService.findOne(id);
-  }
-
-  @Mutation('updateArticle')
-  update(@Args('updateArticleInput') updateArticleInput: UpdateArticleInput) {
-    return this.articleService.update(updateArticleInput.id, updateArticleInput);
-  }
-
-  @Mutation('removeArticle')
-  remove(@Args('id') id: number) {
-    return this.articleService.remove(id);
+    return this.articleService.findOne(id)
   }
 }
--- src/article/dto/update-article.input.ts
+++
@@ -1,6 +0,0 @@
-import { CreateArticleInput } from './create-article.input';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateArticleInput extends PartialType(CreateArticleInput) {
-  id: number;
-}
